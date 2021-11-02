9d089ac211403d505c7cc5b0a59153e15fa938df
Maciej Krysztofiak <maciekkrysz1@gmail.com>
2021-11-02 16:23:40 +0100

    add django-nested-inline for games in admin panel

--- gameserver/settings.py
+++ gameserver/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'channels',
     'games',
     'safedelete',
+    'nested_inline',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
