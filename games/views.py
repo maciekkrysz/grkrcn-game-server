2ba5f8257b26f34637182bcbd3ec87453ab8d2a3
Maciej Krysztofiak <maciekkrysz1@gmail.com>
2021-11-12 21:12:14 +0100

    add some games views

--- games/views.py
+++ games/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
-from django.http import JsonResponse, Http404
+from django.http import JsonResponse, HttpResponseNotFound, HttpResponseBadRequest
+from .classes.games_handler import create_game
 from .models import GameType
 from .resources import normalize_str
-
+from .redis_utils import redis, redis_list_from_dict
+import json
+from django.views.decorators.csrf import ensure_csrf_cookie
 # Create your views here.
 
 
-def room(request, room_id):
+def room(request, game_name, room_id):
     return render(request, 'games/room.html', {
-        'room_id': room_id
+        'room_id': room_id,
+        'game_name': game_name
     })
 
 
@@ -19,9 +23,40 @@ def games(request):
     return JsonResponse(data, safe=False)
 
 
-def games_info(request, game_name):
+def game_lobby(request, game_name):
+    games = redis_list_from_dict('available_games', f'.{game_name}')
+    games.extend(redis_list_from_dict('ongoing_games', f'.{game_name}'))
+    games_to_send = []
+    for game in games:
+        k, v = game.popitem()
+        v['game_id'] = k
+        games_to_send.append(v)
+    return JsonResponse({game_name: games_to_send})
+    pass
+
+
+def game_info(request, game_name):
     for typegame in list(GameType.objects.all().values('type_name', 'description')):
         if normalize_str(typegame['type_name']).lower() == game_name:
             typegame['name'] = game_name
             return JsonResponse(typegame, safe=False)
-    raise Http404("Game does not exist")
+    return HttpResponseNotFound("Game does not exist")
+
+
+@ensure_csrf_cookie
+def game_create(request, game_name):
+    if request.method == 'GET':
+        try:
+            with open(f'games/games_configs/{game_name}.json') as json_file:
+                data = json.load(json_file)
+            return JsonResponse(data)
+        except:
+            return HttpResponseNotFound("Game does not exist")
+    elif request.method == 'POST':
+        # CREATE GAME
+        try:
+            game_id = create_game(game_name, json.loads(request.body))
+            return JsonResponse({'game_id': game_id})
+        except:
+            return HttpResponseBadRequest("Cannot create game")
+    return JsonResponse({})
