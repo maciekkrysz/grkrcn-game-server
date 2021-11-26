cd3385edf537be6fddee1743f1d78090bf5da80e
Maciej Krysztofiak <maciekkrysz1@gmail.com>
2021-11-26 12:17:03 +0100

    add django_setting_module to env

--- gameserver/__init__.py
+++ gameserver/__init__.py
@@ -1,8 +1,9 @@
 from __future__ import absolute_import, unicode_literals
+import django
+from .celery import app as celery_app
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gameserver.settings")
 
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
-import django;
 django.setup()
-
-from .celery import app as celery_app
